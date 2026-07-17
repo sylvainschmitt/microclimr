997b1970c0909a9131a39663ac4e670803323f96
Sylvain Schmitt <sylvain.m.schmitt@gmail.com>
2026-07-17 17:06:38 +0200

    microclimr 0.0.1.9011: renaming arguments, examples, fft_amplitude, fft_phase, ...

--- R/fft.R
+++ R/fft.R
@@ -1,15 +1,18 @@
 #' @importFrom stats fft
 #' @importFrom dplyr arrange reframe mutate full_join rename_with pick
+#' @importFrom dplyr select join_by
 #' @importFrom tidyr unnest
 #' @importFrom tidyselect everything
+#' @importFrom tibble tibble
 #' @importFrom runner runner
+#' @importFrom rlang :=
 NULL
 
 #' Fourier coefficients
 #'
 #' Fast Fourier Transform (FFT) of a series of temperatures.
 #'
-#' @param s num. Sampled temperatures at constant interval.
+#' @param temperatures num. Series of temperatures at constant interval.
 #'
 #' @details
 #'
@@ -46,24 +49,91 @@ NULL
 #'
 #' @examples
 #'
-#' fft_rfft(hobo$t_hobo[1:(24 * 5)])
+#' fft_rfft(temperatures = hobo$t_hobo[1:(24 * 5)])
 #'
-fft_rfft <- function(s) {
-  n <- length(s)
+fft_rfft <- function(temperatures) {
+  n <- length(temperatures)
   l <- ifelse(n %% 2 == 0, (n %/% 2 + 1), ((n + 1) %/% 2))
-  c <- fft(s)[1:l] / n
-  c[2:l] <- c[2:l] * 2
-  return(c) # nolint
+  coeffs <- fft(temperatures)[1:l] / n
+  coeffs[2:l] <- coeffs[2:l] * 2
+  return(coeffs) # nolint
 }
 
-#' FFT frequencies
+#' FFT amplitude
 #'
-#' Fast Fourier Transform (FFT):  frequencies for a real signal of length t (in
+#' Fast Fourier Transform (FFT): get amplitudes from coefficients
+#'
+#' @param coefficients num. The Fourier coefficients.
+#'
+#' @returns
+#'
+#' \eqn{a_{n}} array of amplitudes.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' fft_rfft(temperatures = hobo$t_hobo[1:(24 * 5)]) |>
+#'   fft_amplitude()
+#'
+fft_amplitude <- function(coefficients) {
+  Re(coefficients)
+}
+
+#' FFT phase
+#'
+#' Fast Fourier Transform (FFT): get phases from coefficients
+#'
+#' @param coefficients num. The Fourier coefficients.
+#'
+#' @returns
+#'
+#' \eqn{b_{n}} array of phases.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' fft_rfft(temperatures = hobo$t_hobo[1:(24 * 5)]) |>
+#'   fft_phase()
+#'
+fft_phase <- function(coefficients) {
+  Im(coefficients)
+}
+
+#' FFT Rebuild coefficient
+#'
+#' Fast Fourier Transform (FFT): get coefficients from amplitude and phase
+#'
+#' @param amplitudes num. The Fourier amplitudes
+#' @param phases num. The Fourier phases
+#'
+#' @returns
+#'
+#' \eqn{c} the Fourier coefficients of \eqn{S} of length \eqn{\ell}, whose
+#' coefficients are \eqn{c_{0},c_{1},\ldots,c_{\ell-1}}
+#'
+#' @export
+#'
+#' @examples
+#'
+#' c <- fft_rfft(temperatures = hobo$t_hobo[1:(24 * 5)])
+#' a <- fft_amplitude(c)
+#' p <- fft_phase(c)
+#' fft_coefficient(amplitudes = a, phases = p)
+#'
+fft_coefficient <- function(amplitudes, phases) {
+  complex(real = amplitudes, imaginary = phases)
+}
+
+#' FFT frequency
+#'
+#' Fast Fourier Transform (FFT):  frequencies for a real signal of length T (in
 #' time) with n samples.
 #'
-#' @param n int. Number of samples.
-#' @param t int. Time window, \eqn{t=n\Delta t} where \eqn{\Delta t} is the
-#'   sampling interval.
+#' @param samples int. Number of samples.
+#' @param time_window int. Time window, \eqn{T=n\Delta t} where \eqn{\Delta t}
+#'   is the sampling interval.
 #'
 #' @returns
 #'
@@ -73,18 +143,18 @@ fft_rfft <- function(s) {
 #'
 #' @examples
 #'
-#' fft_freq(201, 10)
+#' fft_freq(samples = 201, time_window = 10)
 #'
-fft_freq <- function(n, t) {
-  seq(1, n %/% 2) / t
+fft_freq <- function(samples, time_window) {
+  seq(1, samples %/% 2) / time_window
 }
 
 #' FFT periods
 #'
-#' Fast Fourier Transform (FFT):  periods for a real signal of length t (in
+#' Fast Fourier Transform (FFT):  periods for a real signal of length T (in
 #' time) with n samples.
 #'
-#' @param f num. The harmonics frequencies.
+#' @param frequencies num. The harmonics frequencies.
 #'
 #' @returns
 #'
@@ -94,22 +164,22 @@ fft_freq <- function(n, t) {
 #'
 #' @examples
 #'
-#' fft_freq(201, 10) |>
+#' fft_freq(samples = 201, time_window = 10) |>
 #'   fft_period()
 #'
-fft_period <- function(f) {
-  1 / f
+fft_period <- function(frequencies) {
+  1 / frequencies
 }
 
 #' FFT mean
 #'
 #' Fast Fourier Transform (FFT): mean temperature
 #'
-#' @param c num. The Fourier coefficients.
+#' @param coefficients num. The Fourier amplitudes.
 #'
 #' @details
 #'
-#' The coefficient \eqn{a_{0}=c_{0}} plays a particular role, it is the mean
+#' The amplitude \eqn{a_{0}} plays a particular role, it is the mean
 #' \deqn{a_{0} = \frac 1 N \sum_{n=1}^{N}S_{n}\,.}
 #'
 #' @returns
@@ -120,18 +190,18 @@ fft_period <- function(f) {
 #'
 #' @examples
 #'
-#' fft_rfft(hobo$t_hobo[1:(24 * 5)]) |>
+#' fft_rfft(temperatures = hobo$t_hobo[1:(24 * 5)]) |>
 #'   fft_mean()
 #'
-fft_mean <- function(c) {
-  Re(c[1])
+fft_mean <- function(coefficients) {
+  Re(coefficients[1])
 }
 
-#' FFT powers
+#' FFT power
 #'
 #' Fast Fourier Transform (FFT): powers
 #'
-#' @param c num. The Fourier coefficients.
+#' @param coefficients num. The Fourier coefficients.
 #'
 #' @details
 #'
@@ -147,18 +217,18 @@ fft_mean <- function(c) {
 #'
 #' @examples
 #'
-#' fft_rfft(hobo$t_hobo[1:(24 * 5)]) |>
-#'   fft_powers()
+#' fft_rfft(temperatures = hobo$t_hobo[1:(24 * 5)]) |>
+#'   fft_power()
 #'
-fft_powers <- function(c) {
-  abs(c[2:length(c)])
+fft_power <- function(coefficients) {
+  abs(coefficients[2:length(coefficients)])
 }
 
 #' FFT energy
 #'
 #' Fast Fourier Transform (FFT): energy
 #'
-#' @param f num. The Fourier coefficients.
+#' @param coefficients num. The Fourier coefficients.
 #'
 #' @details
 #'
@@ -174,18 +244,18 @@ fft_powers <- function(c) {
 #'
 #' @examples
 #'
-#' fft_rfft(hobo$t_hobo[1:(24 * 5)]) |>
+#' fft_rfft(temperatures = hobo$t_hobo[1:(24 * 5)]) |>
 #'   fft_energy()
 #'
-fft_energy <- function(f) {
-  sum(abs(f[2:length(f)])**2) / 2 + Re(f[1])**2
+fft_energy <- function(coefficients) {
+  sum(abs(coefficients[2:length(coefficients)])**2) / 2 + Re(coefficients[1])**2
 }
 
 #' FFT variance
 #'
 #' Fast Fourier Transform (FFT): variance
 #'
-#' @param f num. The Fourier coefficients.
+#' @param coefficients num. The Fourier coefficients.
 #'
 #' @details
 #'
@@ -202,19 +272,19 @@ fft_energy <- function(f) {
 #'
 #' @examples
 #'
-#' fft_rfft(hobo$t_hobo[1:(24 * 5)]) |>
+#' fft_rfft(temperatures = hobo$t_hobo[1:(24 * 5)]) |>
 #'   fft_variance()
 #'
-fft_variance <- function(f) {
-  sum(abs(f[2:length(f)] - f[1])**2) / 2
+fft_variance <- function(coefficients) {
+  sum(abs(coefficients[2:length(coefficients)] - coefficients[1])**2) / 2
 }
 
 #' FFT delay
 #'
 #' Fast Fourier Transform (FFT): delay per frequencies
 #'
-#' @param f num. The Fourier coefficients.
-#' @param freq num. The harmonics frequencies.
+#' @param coefficients num. The Fourier coefficients.
+#' @param frequencies num. The harmonics frequencies.
 #'
 #' @returns
 #'
@@ -224,53 +294,57 @@ fft_variance <- function(f) {
 #'
 #' @examples
 #'
-#' fft_rfft(hobo$t_hobo[1:(24 * 5)]) |>
-#'   fft_delay(fft_freq(24 * 5, 24 * 5))
+#' fft_rfft(temperatures = hobo$t_hobo[1:(24 * 5)]) |>
+#'   fft_delay(fft_freq(samples = 24 * 5, time_window = 24 * 5))
 #'
-fft_delay <- function(f, freq) {
-  Arg(f[2:length(f)]) / (2 * pi * freq)
+fft_delay <- function(coefficients, frequencies) {
+  Arg(coefficients[2:length(coefficients)]) / (2 * pi * frequencies)
 }
 
 #' FFT reconstruct
 #'
 #' Fast Fourier Transform (FFT): Reconstruct the temperatures at time t, whose
-#' spectrum is 'c' associate to frequencies 'f'
+#' coefficients are 'c' associate to frequencies 'f'
 #'
-#' @param c num. The Fourier coefficients.
-#' @param f num. The harmonics frequencies.
-#' @param time num. Time array.
+#' @param coefficients num. The Fourier coefficients.
+#' @param frequencies num. The harmonics frequencies.
+#' @param times num. Time array.
 #'
 #' @returns
 #'
-#' \eqn{s} an array that contains the value of \eqn{s(t)}.
+#' \eqn{s} an array that contains the value of the series of temperature
+#' \eqn{s(t)}.
 #'
 #' @export
 #'
 #' @examples
 #'
 #' fft_reconstruct(
-#'   fft_rfft(hobo$t_hobo[1:(24 * 5)]),
-#'   fft_freq(24 * 5, 24 * 5),
-#'   1:24 * 5
+#'   coefficients = fft_rfft(temperatures = hobo$t_hobo[1:(24 * 5)]),
+#'   frequencies = fft_freq(samples = 24 * 5, time_window = 24 * 5),
+#'   times = 1:24 * 5
 #' )
 #'
-fft_reconstruct <- function(c, f, time) {
-  s <- Re(c[1])
-  for (n in seq_along(f)) {
-    s <- s + Re(c[n + 1]) * cos(2 * pi * f[n] * time) -
-      Im(c[n + 1]) * sin(2 * pi * f[n] * time)
+fft_reconstruct <- function(coefficients,
+                            frequencies,
+                            times) {
+  temperatures <- Re(coefficients[1])
+  for (n in seq_along(frequencies)) {
+    temperatures <- temperatures +
+      Re(coefficients[n + 1]) * cos(2 * pi * frequencies[n] * times) -
+      Im(coefficients[n + 1]) * sin(2 * pi * frequencies[n] * times)
   }
-  return(s) # nolint
+  return(temperatures) # nolint
 }
 
 #' Fourier table
 #'
 #' Fast Fourier Transform (FFT) of a series of temperatures with resulting
-#' frequencies, periods, coefficients and powers in a single table.
+#' frequencies, periods, amplitudes, phase, and powers in a single table.
 #'
-#' @param s num. Sampled temperatures at constant interval.
-#' @param t int. Time window, \eqn{t=N\Delta t} where \eqn{\Delta t} is the
-#'   sampling interval.
+#' @param temperatures num. Series of temperatures at constant interval.
+#' @param time_window int. Time window, \eqn{T=n\Delta t} where \eqn{\Delta t}
+#'   is the sampling interval.
 #' @param period bool. To include period or not, default TRUE.
 #' @param power bool. To include power or not, default TRUE.
 #'
@@ -279,26 +353,29 @@ fft_reconstruct <- function(c, f, time) {
 #' A table with:
 #' * \eqn{f} the harmonics frequencies \eqn{1/t,\ldots \ell/t}
 #' * \eqn{p} the harmonics periods
-#' * \eqn{c} the Fourier coefficients of \eqn{s} of length \eqn{\ell}, whose
-#' coefficients are \eqn{c_{0},c_{1},\ldots,c_{\ell-1}}
+#' * \eqn{a_{n}} the amplitudes of the Fourier coefficients of \eqn{s}
+#' of length \eqn{\ell}, whose values are \eqn{a_{0},a_{1},\ldots,c_{\ell-1}}
+#' * \eqn{b_{n}} the phases of the Fourier coefficients of \eqn{s}
+#' of length \eqn{\ell}, whose values are \eqn{a_{0},a_{1},\ldots,c_{\ell-1}}
 #' * \eqn{P} array of powers, \eqn{P[n] = |c_{n}|} for \eqn{n=1,\ldots,\ell-1}
 #'
 #' @export
 #'
 #' @examples
 #'
-#' fft_tab(hobo$t_hobo[1:(24 * 5)], 24 * 5)
+#' fft_tab(temperatures = hobo$t_hobo[1:(24 * 5)], time_window = 24 * 5)
 #'
-fft_tab <- function(s, t, period = TRUE, power = TRUE) {
-  fc <- fft_rfft(s)
-  freq <- fft_freq(length(s), t)
-  data.frame(
-    frequency = c(0, freq),
-    period = c(0, fft_period(freq)),
-    coefficient = fc,
+fft_tab <- function(temperatures, time_window, period = TRUE, power = TRUE) {
+  coefs <- fft_rfft(temperatures)
+  freqs <- fft_freq(length(temperatures), time_window)
+  tibble(
+    frequency = c(0, freqs),
+    period = c(0, fft_period(freqs)),
+    amplitude = fft_amplitude(coefs),
+    phase = fft_phase(coefs),
     power = c(
-      fft_mean(fc),
-      fft_powers(fc)
+      fft_mean(coefs),
+      fft_power(coefs)
     )
   )
 }
@@ -311,9 +388,9 @@ fft_tab <- function(s, t, period = TRUE, power = TRUE) {
 #'
 #' @param data df. The data frame with a column containing the time index and a
 #'   column containing the temperature.
-#' @param t int. Time window, \eqn{t=N\Delta t} where \eqn{\Delta t} is the
-#'   sampling interval.
-#' @param index_col char. The name of the column containing the time index.
+#' @param time_window int. Time window, \eqn{T=n\Delta t} where \eqn{\Delta t}
+#'   is the sampling interval.
+#' @param time_col char. The name of the column containing the time index.
 #' @param temperature_col char. The name of the column containing the
 #'   temperature series.
 #' @param window time. Window size, can be expressed in time units see
@@ -333,20 +410,25 @@ fft_tab <- function(s, t, period = TRUE, power = TRUE) {
 #' * the datetime of the window
 #' * \eqn{f} the harmonics frequencies \eqn{1/y,\ldots \ell/t}
 #' * \eqn{p} the harmonics periods
-#' * \eqn{f} the Fourier coefficients of \eqn{s} of length \eqn{\ell}, whose
-#' coefficients are \eqn{c_{0},c_{1},\ldots,c_{\ell-1}}
+#' * \eqn{a_{n}} the amplitudes of the Fourier coefficients of \eqn{s}
+#' of length \eqn{\ell}, whose values are \eqn{a_{0},a_{1},\ldots,c_{\ell-1}}
+#' * \eqn{b_{n}} the phases of the Fourier coefficients of \eqn{s}
+#' of length \eqn{\ell}, whose values are \eqn{a_{0},a_{1},\ldots,c_{\ell-1}}
 #' * \eqn{P} array of powers, \eqn{P[n] = |c_{n}|} for \eqn{n=1,\ldots,\ell-1}
 #'
 #' @export
 #'
 #' @examples
 #'
-#' fft_roll(hobo, 24 * 5, "datetime", "t_hobo")
+#' fft_roll(
+#'   data = hobo, time_window = 24 * 5,
+#'   time_col = "datetime", temperature_col = "t_hobo"
+#' )
 #'
 fft_roll <- function(
   data,
-  t,
-  index_col,
+  time_window,
+  time_col,
   temperature_col,
   window = "5 days",
   step = "3 days",
@@ -355,22 +437,24 @@ fft_roll <- function(
 ) {
   .data <- NULL
   data |>
-    arrange(.data[[index_col]]) |>
+    arrange(.data[[time_col]]) |>
     reframe(
       fft = runner(
         x = pick(everything()),
         k = window,
-        at = seq(min(as.data.frame(pick(everything()))[, index_col]),
-          max(as.data.frame(pick(everything()))[, index_col]),
+        at = seq(min(as.data.frame(pick(everything()))[, time_col]),
+          max(as.data.frame(pick(everything()))[, time_col]),
           by = step
         ),
-        idx = index_col,
+        idx = time_col,
         f = function(x) {
-          if (nrow(x) == t) {
-            fft_tab(as.data.frame(x)[, temperature_col], t,
+          if (nrow(x) == time_window) {
+            fft_tab(as.data.frame(x)[, temperature_col], time_window,
               period = period, power = power
             ) |>
-              mutate(datetime = mean(as.data.frame(x)[, index_col]))
+              mutate({{ time_col }} := mean(as.data.frame(x)[, time_col]), #nolint
+                .before = 1
+              )
           }
         }
       )
@@ -388,9 +472,9 @@ fft_roll <- function(
 #'
 #' @param data df. The data frame with a column containing the time index and a
 #'   column containing the temperature.
-#' @param t int. Time window, \eqn{t=N\Delta t} where \eqn{\Delta t} is the
-#'   sampling interval.
-#' @param index_col char. The name of the column containing the time index.
+#' @param time_window int. Time window, \eqn{T=N\Delta t} where \eqn{\Delta t}
+#'   is the sampling interval.
+#' @param time_col char. The name of the column containing the time index.
 #' @param microclimate_col char. The name of the column containing the
 #'   microclimate temperature series.
 #' @param macroclimate_col char. The name of the column containing the
@@ -404,7 +488,7 @@ fft_roll <- function(
 #'
 #' @details
 #'
-#' Can be used with a grouped table, see vignette *to be linked*.
+#' Can be used with a grouped table, see [vignette](Examples.html).
 #'
 #' @returns
 #'
@@ -412,9 +496,10 @@ fft_roll <- function(
 #' * the datetime of the window
 #' * \eqn{f} the harmonics frequencies \eqn{1/y,\ldots \ell/t}
 #' * \eqn{p} the harmonics periods
-#' * \eqn{f} the Fourier coefficients of \eqn{s} of length \eqn{\ell}, whose
-#' coefficients are \eqn{c_{0},c_{1},\ldots,c_{\ell-1}} for micro and
-#' macroclimate.
+#' * \eqn{a_{n}} the amplitudes of the Fourier coefficients of \eqn{s}
+#' of length \eqn{\ell}, whose values are \eqn{a_{0},a_{1},\ldots,c_{\ell-1}}
+#' * \eqn{b_{n}} the phases of the Fourier coefficients of \eqn{s}
+#' of length \eqn{\ell}, whose values are \eqn{a_{0},a_{1},\ldots,c_{\ell-1}}
 #' * \eqn{P} array of powers, \eqn{P[n] = |c_{n}|} for \eqn{n=1,\ldots,\ell-1}
 #' for micro and macroclimate.
 #' * \eqn{R} array of ratios of micro vs. macro-climate powers
@@ -426,16 +511,22 @@ fft_roll <- function(
 #' data <- era |>
 #'   dplyr::rename(era = tas, datetime = time) |>
 #'   dplyr::select(datetime, era) |>
-#'   dplyr::left_join(dplyr::select(hobo, datetime, t_hobo) |>
-#'     dplyr::rename(hobo = t_hobo))
-#' fft_ratio(data, 24 * 5, "datetime", "hobo", "era") |>
+#'   dplyr::left_join(
+#'     dplyr::select(hobo, datetime, t_hobo) |>
+#'       dplyr::rename(hobo = t_hobo),
+#'     by = dplyr::join_by(datetime)
+#'   )
+#' fft_ratio(
+#'   data = data, time_window = 24 * 5, time_col = "datetime",
+#'   microclimate_col = "hobo", macroclimate_col = "era"
+#' ) |>
 #'   dplyr::filter(period == 24) |>
-#'   summary()
+#'   dplyr::summarise_all(mean, na.rm = TRUE)
 #'
 fft_ratio <- function(
   data,
-  t,
-  index_col,
+  time_window,
+  time_col,
   microclimate_col,
   macroclimate_col,
   window = "5 days",
@@ -443,30 +534,31 @@ fft_ratio <- function(
   period = TRUE,
   power = TRUE
 ) {
-  coefficient <- power_micro <- power_macro <- NULL
+  amplitude <- power_micro <- power_macro <- frequency <- NULL
   full_join(
     fft_roll(
       data = data,
-      t = t,
-      index_col = index_col,
+      time_window = time_window,
+      time_col = time_col,
       temperature_col = macroclimate_col,
       window = "5 days",
       step = "3 days",
       period = TRUE,
       power = TRUE
     ) |>
-      rename_with(~ paste0(., "_macro"), .cols = coefficient:power),
+      rename_with(~ paste0(., "_macro"), .cols = amplitude:power),
     fft_roll(
       data = data,
-      t = t,
-      index_col = index_col,
+      time_window = time_window,
+      time_col = time_col,
       temperature_col = microclimate_col,
       window = "5 days",
       step = "3 days",
       period = TRUE,
       power = TRUE
     ) |>
-      rename_with(~ paste0(., "_micro"), .cols = coefficient:power)
+      rename_with(~ paste0(., "_micro"), .cols = amplitude:power),
+    by = join_by(frequency, period, {{ time_col }})
   ) |>
     mutate(ratio = power_micro / power_macro)
 }
--- tests/testthat/test-fft.R
+++ tests/testthat/test-fft.R
@@ -5,7 +5,7 @@ test_that("fft", {
   expect_equal(temp[1], 19.8443750)
   expect_equal(fft_mean(f), 17.6199250)
   expect_equal(fft_period(freq)[1], 120)
-  expect_equal(fft_powers(f)[1], 1.16716029)
+  expect_equal(fft_power(f)[1], 1.16716029)
   expect_equal(fft_energy(f), 317.647568)
   expect_equal(fft_variance(f), 9286.57923)
   expect_equal(fft_delay(f, freq)[1], 24.14719469)
@@ -16,9 +16,11 @@ test_that("fft", {
     dplyr::rename(era = tas, datetime = time) |>
     dplyr::select(datetime, era) |>
     dplyr::left_join(dplyr::select(hobo, datetime, t_hobo) |>
-                       dplyr::rename(hobo = t_hobo))
+                       dplyr::rename(hobo = t_hobo),
+                     by = join_by(datetime))
   expect_s3_class(
     fft_ratio(data, 24 * 5, "datetime", "hobo", "era"),
     "data.frame"
   )
+  expect_equal(fft_coefficient(1,1), 1+1i)
 })
